df8bfdeb9fc7982ba4730be320d2a7e3306c1193
jvolonte <joaquinvolonte@gmail.com>
2026-03-28 14:00:24 -0300

    integrate moon sticker

---
+++ Assets/Scripts/Data/Stickers/MoonStickerData.cs
@@ -0,0 +1,26 @@
+using Stickers;
+using UnityEngine;
+
+namespace Data.Stickers
+{
+    [CreateAssetMenu(menuName = "Stickers/Moon")]
+    public class MoonStickerData : StickerData
+    {
+        public float value;
+
+        public override ISticker Create() => new MoonSticker(value);
+
+        public override string GetDescription()
+        {
+            var odd = "odd";
+            var even = "even";
+            var coloredOdd = RichText.Colorize(odd, ColorService.Odd);
+            var coloredEven = RichText.Colorize(even, ColorService.Even);
+
+            return descriptionTemplate
+                   .Replace($"{odd}", coloredOdd)
+                   .Replace($"{even}", coloredEven)
+                   .Replace("{value}", value.ToString());
+        }
+    }
+}
